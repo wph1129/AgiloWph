f224fc81640d59458fdcb1cbb2cd65c63f733ce7
wph1129 <wangph1230@gmail.com>
2016-12-28 18:29:51 +0800

    modify style
    add table  and  modify style

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -15,8 +15,12 @@ export default class Layout extends React.Component {
                 <Row>
                     <Col xs={12} md={12}> <Header></Header></Col>
                 </Row>
-                <Row>
-                    <Col xs={4} md={2}> <LeftNav></LeftNav></Col>
+                <Row className="show-grid">
+                    <Col xs={4} md={2} style={{
+                        borderRightWidth: '2px',
+                        borderRightStyle: 'solid',
+                        borderRightColor: '#e7e7e7', marginTop: '-20px',height:'100%'
+                    }}> <LeftNav></LeftNav></Col>
                     <Col xs={14} md={10}> {this.props.children} </Col>
                 </Row>
             </Grid>
--- app/routes/Home.jsx
+++ app/routes/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Table, ListGroup, ListGroupItem } from 'react-bootstrap';
+import './Home.css';
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -9,7 +11,61 @@ export default class Home extends React.Component {
   render() {
     return (
       <Layout>
-       <div>我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容我的内容</div>
+        <h5>Active Tickets</h5>
+        <ListGroup >
+          <ListGroupItem className="listItemTips">Item 1</ListGroupItem>
+          <ListGroupItem className="listItemTips">Item 2</ListGroupItem>
+        </ListGroup>
+        <Table striped bordered condensed hover className="table">
+          <thead>
+            <tr>
+              <th>Ticket</th>
+              <th>Summary</th>
+              <th>Component</th>
+              <th>Version</th>
+              <th>Milestone</th>
+              <th>Type</th>
+              <th>Owner</th>
+              <th>Status</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Summary</td>
+              <td>Component</td>
+              <td>Version</td>
+              <td>Milestone</td>
+              <td>Type</td>
+              <td>Owner</td>
+              <td>Status</td>
+              <td>Created</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Summary</td>
+              <td>Component</td>
+              <td>Version</td>
+              <td>Milestone</td>
+              <td>Type</td>
+              <td>Owner</td>
+              <td>Status</td>
+              <td>Created</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Summary</td>
+              <td>Component</td>
+              <td>Version</td>
+              <td>Milestone</td>
+              <td>Type</td>
+              <td>Owner</td>
+              <td>Status</td>
+              <td>Created</td>
+            </tr>
+          </tbody>
+        </Table>
       </Layout>
     );
   }
