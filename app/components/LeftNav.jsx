b03e055d109dc87002c9421fcffe537fda7efe93
wph1129 <wangph1230@gmail.com>
2017-01-16 13:40:36 +0800

    Test   LinkContainer
    LinkContainer  for router-bootstrap

--- app/components/LeftNav.jsx
+++ app/components/LeftNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Nav, NavItem, Label } from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import  './LeftNav.css';
 
 export default class LeftNav extends React.Component {
@@ -22,7 +22,10 @@ export default class LeftNav extends React.Component {
             <div className="leftNav">
                 <h5>Tickets</h5>
                 <Nav bsStyle="pills" stacked activeKey={this.state.currentKey} onSelect={this.handleSelect}>
+    <LinkContainer to="/report/1">
+  
                     <NavItem eventKey={1} href="#">Active Tickets</NavItem>
+  </LinkContainer>
                     <NavItem eventKey={2} href="#">Active Tickets By Version</NavItem>
                     <NavItem eventKey={3} href="#">Active Tickets By Milestone</NavItem>
                     <NavItem eventKey={4} href="#">Accepted,Active Tickets By Owner</NavItem>
